575d571d79f2000e90466ab20d5492f5954957ed
The Running Dev <therunningdev@gmail.com>
2019-11-25 17:11:20 +0100

    Saving the backup just for the future

---
+++ Starter.ServiceBus.backup/RabbitOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Starter.ServiceBus
+{
+    public class RabbitOptions
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string HostName { get; set; }
+
+        public int Port { get; set; } = 5672;
+
+        public string VHost { get; set; } = "/";
+    }
+}
