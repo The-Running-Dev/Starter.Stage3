1817b149f27af3d42bf6cf06860fff91e74a9103
The Running Dev <therunningdev@gmail.com>
2019-11-25 17:09:47 +0100

    Renamed ServiceBus to MessageBus

---
+++ Starter.MessageBus.Consumer/MessageBusConsumer.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+namespace Starter.MessageBus.Consumer
+{
+    public class MessageBusConsumer : BackgroundService
+    {
+        public MessageBusConsumer()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Starter.MessageBus.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Topshelf;
+
+using Starter.Data.Services;
+
+namespace Starter.MessageBus.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var catService = Bootstrapper.IocWrapper.Instance.GetService<ICatService>();
+
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<MessageBusConsumer>(s =>
+                {
+                    s.ConstructUsing(name => new MessageBusConsumer(catService));
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("RabbitMQ Consumer Host");
+                //x.SetDisplayName("Stuff");
+                //x.SetServiceName("Stuff");
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
